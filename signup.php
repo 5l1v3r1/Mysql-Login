f4f62ac11d8f03aebd3080ad5a98c6669a7a0d47
Choudhary Abdullah <magnetonx@gmail.com>
2019-10-21 17:15:26 +0530

    Added Signup

--- signup.php
+++ signup.php
@@ -5,13 +5,13 @@ require_once "database.php";
 <html lang="en">
 <head>
   <meta charset="utf-8">
-  <meta name="description" content="Login">
   <meta name="author" content="Choudhary Abdullah">
+  <meta name="description" content="Sign Up">
   <meta name="keywords" content="Choudhary Abdullah">
   <meta name="theme-color" content="#fff">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Login</title>
-  <link rel="stylesheet" type="text/css" href="login.css">
+  <title>Sign Up</title>
+  <link rel="stylesheet" type="text/css" href="signup.css">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
   <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.15.0/popper.min.js"></script>
@@ -19,31 +19,24 @@ require_once "database.php";
 </head>
 
 <body>
-
   <div class="form">
-  <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
-      <div class="heading"><h2>Login</h2></div>
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+      <div class="heading"><h2>Sign Up</h2></div>
+      <div class="param"><input type="text" name="name" placeholder="Name" class="btn btn-light"></div>
+      <div class="param"><input type="text" name="email" placeholder="Email" class="btn btn-light"></div>
       <div class="param"><input type="text" name="username" placeholder="Username" class="btn btn-light"></div>
-      <div class="param"><input type="password" name="password" placeholder="Password" class="btn btn-light"></div>
+      <div class="param"><input type="text" name="password" placeholder="Password" class="btn btn-light"></div>
       <div class="sbmt"><input type="submit" class="btn btn-success"></div>
-  </form>
-  </div>
+    </form>
 
   <?php
   if($_SERVER["REQUEST_METHOD"] == "POST")
   {
+    $name = $_POST["name"];
+    $email  = $_POST["email"];
     $username = $_POST["username"];
     $password = $_POST["password"];
-    $query = "SELECT id FROM users WHERE username = '$username' and password = '$password'";
+    $query = "INSERT INTO users (name, email, username, password) VALUES ('$name', '$email', '$username', '$password')";
     $result = $conn->query($query);
-    $row = $result->fetch_assoc();
     if (!$result) die("Fatal Error");
-    else if (is_numeric($row["id"]))
-    { ?>
-    <script type="text/javascript">
-       window.open('home.php', '_self');
-    </script>
-  <?php  } } ?>
-
-</body>
-</html>
+  }
