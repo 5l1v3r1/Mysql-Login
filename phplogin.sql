8e88797224c1b1fbbbdfcd100513ec9a587eddcc
Choudhary Abdullah <magnetonx@gmail.com>
2019-10-21 23:39:01 +0530

    Added Favicon

--- phplogin.sql
+++ phplogin.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Oct 21, 2019 at 04:40 PM
+-- Generation Time: Oct 21, 2019 at 06:08 PM
 -- Server version: 5.6.37
 -- PHP Version: 7.1.8
 
@@ -32,18 +32,17 @@ CREATE TABLE IF NOT EXISTS `users` (
   `email` longtext COLLATE utf8_unicode_ci,
   `username` longtext COLLATE utf8_unicode_ci,
   `password` longtext COLLATE utf8_unicode_ci
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `username`, `password`) VALUES
-(1, 'Adil Meraj', 'adilmeraj@gurucool.xyz', 'AdilMeraj', '12345678'),
-(2, 'Rithik Gandhi', 'rithik@cyberstark.org', 'RithikGandhi', '12345678'),
-(3, 'Choudhary Abdullah', 'ceo@cyberstark.org', 'CyberStark', '12345678'),
-(4, 'Yaseen Ammaar', 'yaseen@cyberstark.org', 'YaseenAmmaar', '12345678'),
-(5, 'Osama bin Junaid', 'osama@cyberstark.org', 'IbneJunaid', '12345678');
+(1, 'Choudhary Abdullah', 'ceo@cyberstark.org', 'ChoudharyAbdullah', '25d55ad283aa400af464c76d713c07ad'),
+(2, 'Rithik Gandhi', 'rithik@cyberstark.org', 'RithikGandhi', '25d55ad283aa400af464c76d713c07ad'),
+(3, 'Yaseen Ammaar', 'yaseen@cyberstark.org', 'YaseenAmmaar', '25d55ad283aa400af464c76d713c07ad'),
+(4, 'Osama bin Junaid', 'osama@cyberstark.co', 'IbneJunaid', '25d55ad283aa400af464c76d713c07ad');
 
 --
 -- Indexes for dumped tables
@@ -63,7 +62,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
